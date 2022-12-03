41fcff544bd58278a3c09d8ccd33cb008400db5c
simonecampi <118023444+simonecampi@users.noreply.github.com>
2022-12-03 11:49:15 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,23 @@
+#include "lib.h"
+	bool primo (int n, int i)
+	{
+	    while (i<=n)
+	    {
+	        if (n%i==0 && n==i)
+	        {
+	            return true;
+	        }
+	        else if (n%i==0 && n!=i)
+	        {
+	            return false;
+	        }
+	        else
+	        {
+	            i++;
+	        }
+	
 
+	
+
+	    }
+	}
